1b47a2e949544bb394cd023a47f4396ceb36a44c
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-12-26 20:07:10 -0300

    creating security group for ALB

---
+++ ecs_fargate/2-platform/securitygroups.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "ecs-alb-security-group" {
+  name        = "${var.ecs_cluster_name}-ALB-SG"
+  description = "Security Group for ALB to traffic for ECS cluster"
+  vpc_id = "${data.terraform_remote_state.infrastructure.vpc_id}"
+
+  ingress = [ {
+    cidr_blocks = [ "${var.internet_cidr_blocks}" ]
+    description = "Traffic from internet"
+    from_port   = 443
+    protocol    = "TCP"
+    to_port     = 443
+  } ]
+
+  egress = [ {
+    cidr_blocks = [ "${var.internet_cidr_blocks}" ]
+    description = "Traffic from ALB to internet"
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  } ]
+}
--- ecs_fargate/2-platform/variables.tf
+++ ecs_fargate/2-platform/variables.tf
@@ -8,3 +8,5 @@ variable "remote_state_bucket" {}
 variable "remote_state_key" {}
 
 variable "ecs_cluster_name" {}
+
+variable "internet_cidr_blocks" {}
