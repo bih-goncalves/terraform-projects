51fee0c0f8dd2380364921f6f84e46a481dab00e
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-12-26 20:07:33 -0300

    finishing ALB definition

--- ecs_fargate/2-platform/ecs.tf
+++ ecs_fargate/2-platform/ecs.tf
@@ -21,7 +21,12 @@ resource "aws_ecs_cluster" "production-fargate-cluster" {
 }
 
 resource "aws_elb" "ecs-cluster-elb" {
-  name     = "${var.ecs_cluster_name}-ALB"
-  internal = false
-}
+  name            = "${var.ecs_cluster_name}-ALB"
+  internal        = false
+  security_groups = [ "${aws_security_group.ecs-alb-security-group.id}" ]
+  subnets         = [ "${split(",", join(",", data.terraform_remote_state.infrastructure.public_subnets))}" ]
 
+  tags = {
+    "Name" = "${var.ecs_cluster_name}-ALB"
+  }
+}
