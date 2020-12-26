10a6642ec6de4564e69e2cd1ebfa3d325a36036a
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-12-26 20:21:41 -0300

    create a domain definition

---
+++ ecs_fargate/2-platform/domain.tf
@@ -0,0 +1,22 @@
+resource "aws_acm_certificate" "ecs-domain-certificate" {
+  domain_name       = "*.${var.ecs_domain_name}"
+  validation_method = "DNS"
+
+  tags = {
+    "Name" = "${var.ecs_cluster_name}-Certificate"
+  }
+}
+
+data "aws_route53_zone" "ecs-domain" {
+  name = "${var.ecs_domain_name}"
+  private_zone = false
+}
+
+resource "aws_route53_record" "ecs-cert-validation-record" {
+  name    = "${aws_acm_certificate.ecs-domain-certificate.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.ecs-domain-certificate.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.ecs-domain.zone_id}"
+  records = ["${aws_acm_certificate.ecs-domain-certificate.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+  allow_overwrite = true
+}
--- ecs_fargate/2-platform/securitygroups.tf
+++ ecs_fargate/2-platform/securitygroups.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "ecs-alb-security-group" {
   name        = "${var.ecs_cluster_name}-ALB-SG"
   description = "Security Group for ALB to traffic for ECS cluster"
-  vpc_id = "${data.terraform_remote_state.infrastructure.vpc_id}"
+  vpc_id      = "${data.terraform_remote_state.infrastructure.vpc_id}"
 
   ingress = [ {
     cidr_blocks = [ "${var.internet_cidr_blocks}" ]
