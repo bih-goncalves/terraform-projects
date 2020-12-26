c73f784d8a350a341746c79e591a30cc0d87bd33
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-12-26 20:35:22 -0300

    create ALB target group

--- ecs_fargate/2-platform/ecs.tf
+++ ecs_fargate/2-platform/ecs.tf
@@ -31,6 +31,17 @@ resource "aws_alb" "ecs-cluster-alb" {
   }
 }
 
+resource "aws_lb_target_group" "ecs-default-target-group" {
+  name     = "${var.ecs_cluster_nmae}-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${data.terraform_remote_state.infrastructure.vpc_id}"
+
+  tags = {
+    "Name" = "${var.ecs_cluster_name}-TG"
+  }
+}
+
 resource "aws_route53_record" "ecs-load-balancer-record" {
   name    = "*.${var.ecs_domain_name}"
   type    = "A"
