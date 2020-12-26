4f67fc2bdd04ea2ded439543070acd92cf9863ab
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-12-26 17:54:31 -0300

    creating private subnets associations

--- ecs_fargate/1-infrastructure/vpc.tf
+++ ecs_fargate/1-infrastructure/vpc.tf
@@ -91,16 +91,30 @@ resource "aws_route_table" "private-route-table" {
 
 resource "aws_route_table_association" "public-subnet-1-association" {
   route_table_id = "${aws_route_table.public-route-table.id}"
-  subnet_id = "${aws_subnet.public-subnet-1.id}"
+  subnet_id      = "${aws_subnet.public-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "public-subnet-2-association" {
   route_table_id = "${aws_route_table.public-route-table.id}"
-  subnet_id = "${aws_subnet.public-subnet-2.id}"
+  subnet_id      = "${aws_subnet.public-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "public-subnet-3-association" {
   route_table_id = "${aws_route_table.public-route-table.id}"
-  subnet_id = "${aws_subnet.public-subnet-3.id}"
+  subnet_id      = "${aws_subnet.public-subnet-3.id}"
 }
 
+resource "aws_route_table_association" "private-subnet-1-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-1.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-3-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-3.id}"
+}
