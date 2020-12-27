d85c363257bbc24aca1862bab4adbf8b5a2275c0
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-12-26 23:58:19 -0300

    create iam and iam policy for fargate

--- ecs_fargate/3-application/infrastructure/app.tf
+++ ecs_fargate/3-application/infrastructure/app.tf
@@ -40,3 +40,44 @@ resource "aws_ecs_task_definition" "springbootapp-task-definition" {
   execution_role_arn       = ""
   task_role_arn            = ""
 }
+
+resource "aws_iam_role" "fargate-iam-role" {
+  name               = "${var.ecs_service_name}-IAM-Role"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+    {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": ["ecs.amazonaws.com", "ecs-tasks.amazonaws.com"]
+        },
+        "Action": "sts:AssumeRole"
+    }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "fargate-iam-role-policy" {
+  name = "${var.ecs_service_name}-IAM-Role-Policy"
+  role = "${aws_iam_role.fargate-iam-role.id}"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecs:*",
+                "ecr:*",
+                "logs:*",
+                "cloudwatch:*",
+                "elasticloadbalancing:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
